9dea56e8dcef225c12d2cd51202e9a647d2b476f
Kalka <k@kalka.io>
2022-05-02 03:29:35 -0400

    commit initiale

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+import setuptools
+
+setuptools.setup(
+    name="wolfpackutil",
+    version="0.1.1",
+    author="Kalka",
+    author_email="kalka2088@gmail.com",
+    description="Python util library for WolfpackMC packages. https://github.com/WolfpackMC",
+    url="https://github.com/WolfpackMC/wolfpackutil",
+    project_urls={
+        "Bug Tracker": "https://github.com/WolfpackMC/wolfpackutil/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.10",
+    install_requires=[
+        "owoify-py>=1.1.2",
+        "pyfiglet>=0.8.post1",
+        "python-dateutil>=0.8.post1",
+        "rich>=12.3.0"
+    ]
+)
---
+++ src/wolfpackutil/__init__.py
@@ -0,0 +1,8 @@
+class WolfpackUtil:
+    url = "https://addons-ecs.forgesvc.net"
+    github_url = "https://api.github.com/repos/{}/{}/releases"
+    author = "WolfpackMC"
+    repo = ""
+    headers = {'User-Agent': 'WolfpackMC (made by Kalka) business inquiries: b@kalka.io'}
+
+
